5e1c78e22b215932977206ea6043bad483fc20a8
Alif Zulfanur <2ez4second@gmail.com>
2023-04-13 12:17:47 +0700

    yagitudeh

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,103 +3,77 @@ import Bounce from "react-reveal/Bounce";
 import card from "./images/card-top.jpg";
 
 function Project() {
+  let projects = [
+    {
+      name: "Contact",
+      link: "/contact",
+      description: "Projek Aplikasi",
+      image: card,
+      tag: "Flutter App",
+    },
+    {
+      name: "Project",
+      link: "/project",
+      description: "Projek Website",
+      image: card,
+      tag: "Website App",
+    },
+    {
+      name: "Project",
+      link: "/project",
+      description: "Projek Website",
+      image: card,
+      tag: "Website App",
+    },
+    {
+      name: "Project",
+      link: "/project",
+      description: "Projek Website",
+      image: card,
+      tag: "Website App",
+    },
+    {
+      name: "Project",
+      link: "/project",
+      description: "Projek Website",
+      image: card,
+      tag: "Website App",
+    },
+  ];
   return (
-    <div className="w-full h-screen p-2">
-      <div className="w-full h-full p-2 mx-auto flex justify-start items-center">
-        <div className=" mx-auto">
-          <div className="justify-start">
-            <Bounce left>
-            <h1 className="font-semibold duration-100 mt-4 text-2xl font-poppins text-[#eebbc3]">
-              <span className="text-white">My</span> Project :
-            </h1>
-            <br></br>         
-              <div className="w-full h-auto mx-auto grid-cols-4 gap-4 flex flex-wrap">
-                <div class="max-w-sm rounded overflow-hidden shadow-lg  bg-white">
-                  <img
-                    class="w-full"
-                    src={card}
-                    alt="Sunset in the mountains"
-                  ></img>
-                  <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-                    <p class="text-gray-700 text-base">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                      Voluptatibus quia, nulla! Maiores et perferendis eaque,
-                      exercitationem praesentium nihil.
-                    </p>
-                  </div>
-                  <div class="px-6 pt-4 pb-2">
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                      #photography
-                    </span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                      #travel
-                    </span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                      #winter
-                    </span>
-                  </div>
-                </div>
-
-                <div class="max-w-sm rounded overflow-hidden shadow-lg  bg-white">
-                  <img
-                    class="w-full"
-                    src={card}
-                    alt="Sunset in the mountains"
-                  ></img>
-                  <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-                    <p class="text-gray-700 text-base">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                      Voluptatibus quia, nulla! Maiores et perferendis eaque,
-                      exercitationem praesentium nihil.
-                    </p>
-                  </div>
-                  <div class="px-6 pt-4 pb-2">
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                      #photography
-                    </span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                      #travel
-                    </span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                      #winter
-                    </span>
-                  </div>
-                </div>
-
-                <div class="max-w-sm rounded overflow-hidden shadow-lg  bg-white">
-                  <img
-                    class="w-full"
-                    src={card}
-                    alt="Sunset in the mountains"
-                  ></img>
-                  <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-                    <p class="text-gray-700 text-base">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                      Voluptatibus quia, nulla! Maiores et perferendis eaque,
-                      exercitationem praesentium nihil.
-                    </p>
-                  </div>
-                  <div class="px-6 pt-4 pb-2">
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                      #photography
-                    </span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                      #travel
-                    </span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                      #winter
-                    </span>
-                  </div>
-                </div>
-              </div>
-          </Bounce>
-            
-            
+    <div className=" h-auto mt-32 lg:mt-0">
+      <div className="container  mx-auto w-full h-full p-2 flex justify-start items-center">
+        <div className="p-2 mx-auto">
+          <Bounce left>
+            <div className=" w-full h-auto just flex items-center">
+              <h1 className="font-semibold duration-100 p-2 text-2xl font-poppins tracking-widest text-[#eebbc3]">
+                My <span className="text-white">Project</span>
+              </h1>
+            </div>
             <br></br>
-          </div>
+            <div className="w-full h-auto mx-auto justify-center p-2 grid-cols-4 gap-4 flex flex-wrap">
+              {projects.map((e) => (
+                <div class="max-w-sm  p-2 rounded overflow-hidden shadow-lg  bg-white">
+                  <img
+                    class="w-full"
+                    src={e.image}
+                    alt="Sunset in the mountains"
+                  ></img>
+                  <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">{e.name}</div>
+                    <p class="text-gray-700 text-base">{e.description}</p>
+                  </div>
+                  <div class="px-6 pt-4 pb-2">
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                      {e.tag}
+                    </span>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </Bounce>
+
+          <br></br>
         </div>
       </div>
     </div>
