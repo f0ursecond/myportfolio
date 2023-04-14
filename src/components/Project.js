8939af6790e31fc6a18b6dec0c23e2e11fcbac54
Alif Zulfanur <2ez4second@gmail.com>
2023-04-14 13:02:50 +0700

    yaaa

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,49 +1,55 @@
 import React from "react";
 import Bounce from "react-reveal/Bounce";
 import card from "./images/card-top.jpg";
-import cursed from './images/cursed.png';
+import cursed from "./images/okee.png";
 
 function Project() {
   let projects = [
     {
       name: "The Cursed Night At School",
       link: "https://cursed-night.netlify.app/",
-      description: "Project membuat website untuk tugas akhir sekolah yaitu project game. Disini saya sebagai frontend developer ",
+      description:
+        "Project membuat website untuk tugas akhir sekolah yaitu project game. Disini saya sebagai frontend developer ",
       image: cursed,
       tag: "React JS & Tailwindcss",
     },
     {
       name: "Al Quran App",
       link: "https://github.com/f0ursecond/quran_app",
-      description: "Projek Aplikasi Android yaitu membuat AL Quran App, ini sebenarnya belom 100% jadi karena saya hanya mencoba untuk fetch API dari internet saja",
+      description:
+        "Projek Aplikasi Android yaitu membuat AL Quran App, ini sebenarnya belom 100% jadi karena saya hanya mencoba untuk fetch API dari internet saja",
       image: card,
       tag: "Flutter",
     },
     {
       name: "Login Page with Firebase",
       link: "https://github.com/f0ursecond/login-ui-firebase",
-      description: "Projek Aplikasi Android membuat page login dengan memadukan authentication menggunakan Firebase",
+      description:
+        "Projek Aplikasi Android membuat page login dengan memadukan authentication menggunakan Firebase",
       image: card,
       tag: "Flutter",
     },
     {
       name: "CRUD menggunakan PHP Native",
       link: "https://github.com/f0ursecond/cafe",
-      description: "Projek Website sekolah membuat smart cafe, menggunakan php native dan Tailwind CSS",
+      description:
+        "Projek Website sekolah membuat smart cafe, menggunakan php native dan Tailwind CSS",
       image: card,
       tag: "Tailwindcss & Php Native",
     },
     {
       name: "Simple login page UI",
       link: "/https://github.com/f0ursecond/loginapp",
-      description: "Projek Aplikasi login sederhana, hanya menggunakan if else saja , karena ini pertama kali saya menggunakan android studio dan java",
+      description:
+        "Projek Aplikasi login sederhana, hanya menggunakan if else saja , karena ini pertama kali saya menggunakan android studio dan java",
       image: card,
       tag: "Java & Android Studio",
     },
     {
       name: "Membuat Rest API menggunakan express dan sequelize",
       link: "https://github.com/f0ursecond/rest-api",
-      description: "Projek restapi sederhana, untuk sekadar penasaran bagaimana sebenarnya api bisa terbuat",
+      description:
+        "Projek restapi sederhana, untuk sekadar penasaran bagaimana sebenarnya api bisa terbuat",
       image: card,
       tag: "Javascript & NodeJS & Express & Mysql",
     },
@@ -69,7 +75,13 @@ function Project() {
                   ></img>
                   <div class="px-6 py-4">
                     <div class="font-bold text-xl mb-2">
-                      <a className="hover:text-sky-500" href={e.link} target="blank">{ e.name }</a>
+                      <a
+                        className="hover:text-sky-500"
+                        href={e.link}
+                        target="blank"
+                      >
+                        {e.name}
+                      </a>
                     </div>
                     <p class="text-gray-700 text-base">{e.description}</p>
                   </div>
