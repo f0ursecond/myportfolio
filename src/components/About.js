a47febb4f7421d6ca701cdd7c98e5b2087743179
f0ursecond <2ez4second@gmail.com>
2023-04-17 12:25:56 +0700

    final

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,16 @@
 import React from "react";
 import Zoom from "react-reveal/Zoom";
 
+import {
+  FaGitAlt,
+  FaJs,
+  FaAndroid,
+  FaLinkedin,
+  FaGithub,
+  FaInstagram,
+} from "react-icons/fa";
+import { SiDart } from "react-icons/si";
+
 function About() {
   return (
     <div className="w-full h-full ">
@@ -12,10 +22,11 @@ function About() {
                 About Me :
               </h1>
               <h6 className="font-semibold text-2xl font-poppins text-[#eebbc3]  border-b-2 ">
-                <span className="text-white">My Name is</span> Alif Zulfanur Said Ramadhan
+                <span className="text-white">My Name is</span> Alif Zulfanur
+                Said Ramadhan
               </h6>
               <br></br>
-              
+
               <ul>
                 <li className="list-disc text-white">
                   <p className="font-normal text-base font-poppins text-left  text-[#fffffe]">
@@ -28,42 +39,22 @@ function About() {
                       mempunyai ketertarikan di bidang Komputer dan saya suka
                       mendalami Hardware & Software komputer juga . Saya saat
                       ini sedang fokus mempelajari Mobile Developer terutama
-                      Flutter<br></br>
-                      <br></br>
-                      Cupidatat cillum commodo culpa laborum qui tempor sunt.
-                      Minim adipisicing irure consequat fugiat tempor dolor
-                      fugiat in enim. Deserunt aliquip incididunt amet aute
-                      deserunt aliqua ad in excepteur ex. Deserunt ex qui ea
-                      enim cupidatat voluptate pariatur consectetur aliquip
-                      tempor exercitation dolor.Officia occaecat id sit commodo
-                      nostrud do officia do sit laborum. Voluptate consequat
-                      excepteur laborum do minim aute culpa. Reprehenderit
-                      cillum occaecat do duis nostrud anim mollit ea cupidatat
-                      Lorem sit cillum est. Duis commodo do aliquip irure
-                      proident sint exercitation amet fugiat. Sunt consectetur
-                      cupidatat esse mollit dolore mollit aliquip sit esse
-                      dolore ipsum. Ullamco excepteur labore qui cupidatat sint
-                      proident voluptate mollit anim. Velit nostrud id dolor sit
-                      sit cupidatat elit laboris commodo aliquip voluptate
-                      adipisicing anim.Id fugiat cupidatat sit sit amet ex ex
-                      magna laborum. Commodo ad irure adipisicing mollit. Labore
-                      laborum esse velit ex nostrud aliquip reprehenderit ipsum
-                      occaecat ea.Aute qui cupidatat magna do aliquip
-                      incididunt. Irure dolore sit non duis consectetur
-                      cupidatat fugiat proident amet ex pariatur dolore aliquip
-                      do. Ipsum ut id aute magna deserunt. Laboris amet laborum
-                      nisi nostrud non ut duis deserunt laboris pariatur et
-                      nulla mollit ullamco. Occaecat irure et nostrud excepteur
-                      sit magna Lorem consectetur do.Officia nulla ullamco
-                      aliqua in ut officia ea id laboris do incididunt sit.
-                      Labore deserunt ut labore eiusmod. Aute amet sint eu anim
-                      tempor culpa amet ea aliqua.Do cupidatat id deserunt
-                      ullamco fugiat aliquip ad exercitation. Excepteur Lorem ex
-                      laborum in ut ullamco exercitation nisi consectetur. Magna
-                      sunt pariatur ipsum Lorem. Irure nostrud veniam irure amet
-                      aliquip non do anim. Mollit eiusmod officia in commodo
-                      culpa dolore. Ut amet fugiat anim eu pariatur. Aute fugiat
-                      consequat et laborum qui irure occaecat veniam id culpa.{" "}
+                      Flutter. Saya mempunyai ketertarikan sejak kecil dengan
+                      dunia Komputer. Saya mulai mendalami komputer di kelas 8
+                      SMP dengan modal browsing di youtube, saat itu saya di
+                      sarankan oleh paman saya untuk mencoba membuat database
+                      mysql, lalu saya mencobanya dan disitu adalah titik awal
+                      saya mulai di dunia programming. Setelah itu saya mulai
+                      menambah ilmu dengan belajar c++ di youtube Kelas Terbuka.
+                      Dan setelah itu saya tidak menyentuh programming lagi
+                      dikarenakan laptop saya rusak. Setelah berjalanya waktu
+                      saya masuk smk dan orang tua mempunyai rezeki untuk
+                      membelikan saya komputer dan laptop , mulai saat itu saya
+                      langsung mencoba Mobile Development , karena untuk Web
+                      Development sudah banyak teman teman saya yang minat
+                      kesitu , makanya saya mencoba berbeda. Sampailah di titik
+                      sekarang saya sudah menemukan bakat minat saya yaitu di
+                      Mobile Development terutama menggunakan Framework Flutter.
                       <br></br>
                       <br></br>
                     </p>
@@ -72,27 +63,40 @@ function About() {
                 <li className="list-disc text-white">
                   <p className="font-normal text-base font-poppins text-left  text-[#eebbc3]">
                     <span className="font-poppins font-bold text-2xl text[#fffffe] -tracking-tighter">
-                      Experience & Education :{" "}
+                      Language & Tools :{" "}
                     </span>
                     <br></br>
                   </p>
-                  <p className="font-normal text-base font-poppins text-left  text-[#fffffe]">
-                    Lorem nostrud aute aliqua voluptate. Eu minim nostrud ea
-                    minim id veniam. Eiusmod mollit ea dolore fugiat ullamco
-                    sint ex Lorem id aute labore. Nisi id ex amet non quis
-                    et.Esse magna velit commodo ex fugiat. Et consectetur est
-                    fugiat nulla labore voluptate duis culpa fugiat ullamco
-                    officia. Est aute aliqua pariatur tempor est occaecat nulla
-                    consectetur aute deserunt magna non. Excepteur ipsum id esse
-                    id ipsum minim. Sit ullamco ea sit pariatur commodo irure
-                    ullamco ea elit.Aliqua id ullamco cupidatat voluptate
-                    pariatur eu ad consequat aliqua excepteur ea culpa veniam.
-                    Elit consequat commodo sunt fugiat pariatur reprehenderit
-                    incididunt est deserunt. Sunt dolore velit voluptate do
-                    minim occaecat deserunt enim Lorem excepteur. Mollit veniam
-                    aliqua est veniam esse ipsum dolor eu duis. Esse irure nisi
-                    culpa anim in.
+                  <div className="mx-auto  h-20 p-4">
+                    <div className=" lg:flex block justify-between ">
+                      <FaJs className="h-12 w-12" />
+                      <FaGitAlt className="h-12 w-12" />
+                      <FaAndroid className="h-12 w-12" />
+                      <SiDart className="h-12 w-12" />
+                    </div>
+                  </div>
+                </li>
+                <li className="list-disc text-white">
+                  <p className="font-normal text-base font-poppins text-left  text-[#eebbc3]">
+                    <span className="font-poppins font-bold text-2xl text[#fffffe] -tracking-tighter">
+                      Social Media :{" "}
+                    </span>
+                    <br></br>
                   </p>
+                  <div className="mx-auto  h-20 p-4">
+                    <div className=" lg:flex block justify-between ">
+                      <a target="blank" href="https://github.com/f0ursecond">
+                        <FaGithub className="h-12 w-12" />
+                      </a>
+                      <a target="blank" href="https://www.linkedin.com/in/azulfanur/">
+                        <FaLinkedin className="h-12 w-12" />
+                      </a>
+                      
+                      <a target="blank" href="https://instagram.com/a.zulfanur_">
+                        <FaInstagram className="h-12 w-12" />
+                      </a>
+                    </div>
+                  </div>
                 </li>
               </ul>
             </div>
